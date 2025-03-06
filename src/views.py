acf4cc051c60f1a1f68a0608efc0a58a4efe656f
Alexei <streiker.saik@gmail.com>
2025-03-06 12:38:55 +0300

    выполнена функция views_home + test

--- src/views.py
+++ src/views.py
@@ -2,19 +2,12 @@ import datetime
 import json
 import logging
 import os
-from typing import Any, Dict, List
 
 import pandas as pd
 
-from src.utils import (
-    filter_operations_by_month_and_date,
-    generate_card_report,
-    get_currencies_rates_in_rub,
-    get_stocks_in_usd,
-    get_transactions_from_excel,
-    get_user_settings_from_json,
-    greeting_from_time_to_time,
-)
+from src.utils import (filter_operations_by_month_and_date, generate_card_report, generator_top_five_transactions,
+                       get_currencies_rates_in_rub, get_stocks_in_usd, get_transactions_from_excel,
+                       get_user_settings_from_json, greeting_from_time_to_time)
 
 # создание абсолютного пути из относительного
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -48,29 +41,29 @@ def views_home(date: str, file_operations: str, file_user_settings: str) -> str:
     cards = generate_card_report(filter_df)
 
     # 3. Топ - 5 транзакций по сумме платежа
-    top_transactions = []
+    top_transactions = generator_top_five_transactions(filter_df)
 
-    # # 4. Курс валют
-    # currencies = user_settings[0].get("user_currencies", [])
-    # currency_rates = get_currencies_rates_in_rub(currencies)
-    #
-    # # 5. Стоимость акций из S&P500
-    # stocks_list = user_settings[0].get("user_stocks", [])
-    # stock_prices = get_stocks_in_usd(stocks_list)
+    # 4. Курс валют
+    currencies = user_settings[0].get("user_currencies", [])
+    currency_rates = get_currencies_rates_in_rub(currencies)
+
+    # 5. Стоимость акций из S&P500
+    stocks_list = user_settings[0].get("user_stocks", [])
+    stock_prices = get_stocks_in_usd(stocks_list)
 
     data_output = {
         "greeting": greeting,
         "cards": cards,
-        # "top_transactions": top_transactions,
-        # "currency_rates": currency_rates,
-        # "stock_prices": stock_prices,
+        "top_transactions": top_transactions,
+        "currency_rates": currency_rates,
+        "stock_prices": stock_prices,
     }
     return json.dumps(data_output, indent=4, ensure_ascii=False)
 
 
-if __name__ == "__main__":
-    date = "2021-12-21 12:00:00"
-    file_operations = "../data/operations.xlsx"
-    file_user_settings = "../user_settings.json"
-    result = views_home(date, file_operations, file_user_settings)
-    print(result)
+# if __name__ == "__main__":
+#     date = "2021-12-21 12:00:00"
+#     file_operations = "../data/operations.xlsx"
+#     file_user_settings = "../user_settings.json"
+#     result = views_home(date, file_operations, file_user_settings)
+#     print(result)
